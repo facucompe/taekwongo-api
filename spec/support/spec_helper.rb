177b0defac52b3fcfc0edb581950b55f91045ec4
facu <facu.compe@gmail.com>
2018-03-29 22:08:40 -0300

    Initial commit

---
+++ spec/support/spec_helper.rb
@@ -0,0 +1,30 @@
+require 'simplecov'
+SimpleCov.start
+require 'factory_bot'
+require 'faker'
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:all) do
+    FactoryBot.reload
+  end
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+    Timecop.return
+  end
+end
