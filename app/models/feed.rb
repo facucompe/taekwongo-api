f54847e85f51088f0f60bc72dcea2a977e44dbbd
facucompe <facu.compe@gmail.com>
2018-07-14 19:59:23 -0300

    Change feeds categories

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,4 +1,4 @@
 class Feed < ApplicationRecord
   validates :title, :picture_url, presence: true
-  enum category: { tournament: 0, rules: 1, arbitration: 2, other: 3 }
+  enum category: { refereeing: 0, rules: 1, general: 2 }
 end
