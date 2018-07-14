22cb69724794f3ae521274f58d97d7254af26761
facucompe <facu.compe@gmail.com>
2018-07-14 19:24:21 -0300

    Add register for feeds

--- app/admin/feeds.rb
+++ app/admin/feeds.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Feed do
-
+  index do
+    id_column
+    column :title
+    column :picture_url
+    column :category
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
