6eacbf9823b2b1fd17b38c06e5aebb99766ab3a7
Marco Di Croce <marcodicro@hotmail.com>
2018-07-26 18:35:06 -0300

    Devuelvo solo competiciones cuya fecha de inicio sea mayor a la fecha actual

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -2,6 +2,6 @@ class CompetitionsController < ApiController
   skip_before_action :current_user, :authenticate_request, except: [:edit_device_token]
 
   def index
-    render json: Competition.all
+    render json: Competition.where('start_date > ?', Date.today)
   end
 end
