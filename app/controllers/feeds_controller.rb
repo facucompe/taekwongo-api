f10b3478a269cf21df226b7789aa7f6b50a27603
Felipe <feliress13@gmail.com>
2018-09-05 23:04:58 -0300

    agrego filtro al endpint de feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,6 +2,13 @@ class FeedsController < ApiController
   skip_before_action :current_user, :authenticate_request, except: [:edit_device_token]
 
   def index
-    render json: Feed.all
+    byebug
+    render json: Feed.where(category: feed_param[:category])
+  end
+
+  private
+
+  def feed_param
+    params.permit(:category)
   end
 end
