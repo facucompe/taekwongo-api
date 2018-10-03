6f1c8bb4c1fe6957c4407e1c31e231f8cd619ad9
facucompe <facu.compe@gmail.com>
2018-10-02 23:24:44 -0300

    add body to serializer

--- app/serializers/feed_serializer.rb
+++ app/serializers/feed_serializer.rb
@@ -1,8 +1,11 @@
 class FeedSerializer < ActiveModel::Serializer
-  attributes :id, :title, :picture_url, :category
-  has_many :feed_images
+  attributes :id, :title, :picture_url, :category, :feed_images, :body
 
   def picture_url
     object.feed_images.first&.link&.url
+  end
+
+  def feed_images
+    object.feed_images[1..-1]
   end
 end
--- spec/factories/feed.rb
+++ spec/factories/feed.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :feed do
     title { Faker::Book.title }
     body { Faker::Lorem.sentence(10) }
-    picture_url { Faker::Internet.url }
     category { Feed.categories.keys.sample }
   end                 
 end
